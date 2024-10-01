ad2a79530a01a7aba9d34daa2e5e5e204be75551
Nikki Barry <nikkibarry@acm.org>
2024-10-01 14:17:50 -0700

    feat: Add fun plugins
    - Add fun plugins
    - Add mise integration

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -41,4 +41,8 @@ vim.opt.spell = true
 -- Set EOL
 vim.o.fixeol = true
 
+-- Set scrolloff
 vim.o.scrolloff = 12
+
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
---
+++ lua/plugins/fun.lua
@@ -0,0 +1,8 @@
+return {
+	{
+		"eandrju/cellular-automaton.nvim",
+	},
+	{
+		"letieu/hacker.nvim",
+	},
+}
